e361165659ccb671790a64982c5be60af1971cb8
DarkWebChannel <120795237+DarkWebChannel@users.noreply.github.com>
2024-04-25 21:05:33 +0100

    Add files via upload

---
+++ kvpngensourse.py
@@ -0,0 +1,34 @@
+﻿import requests
+print("made by KEF, with love❤<3")
+
+url = 'https://hidemy.name/ru/demo/'
+
+if 'Ваша электронная почта' in requests.get(url).text:
+    
+    email = input('Введите электронную почту для получения ключа: ')
+
+    response = requests.post('https://hidemy.name/ru/demo/success/', data={
+        "demo_mail": f"{email}"
+    })
+
+    if 'Ваш код выслан на почту' in response.text:
+        confirm = input('Введите полученную ссылку для подтверждения e-mail адреса: ')
+        
+        while True:
+            try:
+                response = requests.get(confirm)
+                if 'Спасибо' in response.text:
+                    print('Почта подтверждена. Код отправлен на вашу почту!')
+                    break
+                else:
+                    confirm = input('Ссылка невалидная, повторите попытку: ')
+            except:
+                confirm = input('Ссылка невалидная, повторите попытку: ')
+                continue
+
+
+    else:
+        print('Указанная почта не подходит для получения тестового периода ')
+
+else:
+    print('Невозможно получить тестовый период, попробуйте позже...')
